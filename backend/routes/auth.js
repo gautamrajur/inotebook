a3501423a667de4646339e925d86be077eeb26ad
gautam <reachgautam10@gmail.com>
2023-01-08 22:21:31 +0530

    createIndexes() function applied on User Schema

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/User")
+const { body, validationResult } = require('express-validator');
 
 
-//Create a User using : POST "/api/auth". Doesn't require auth
-router.post('/', (req, res)=> {
-    console.log(req.body)
-    const user = User(req.body);
-    user.save()
-    res.json()
+//Create a User using : POST "/api/auth". No login required
+router.post('/',[  
+    body('email', 'Enter a valid email').isEmail(),
+    // password must be at least 5 chars long
+    body('password','Password needs to be atleast 5 characters').isLength({ min: 5 }),
+    body('name','Enter a name with atleast 3 characters').isLength({ min: 3 })], (req, res)=> {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) 
+          return res.status(400).json({ errors: errors.array() });
 
+          User.create({
+            name : req.body.name,
+            password : req.body.password,
+            email : req.body.email
+        }).then(user => res.json(user)).catch(err => {console.log(err)
+        res.json('Enter a unique email value')});
+        
 })
+  
 
 module.exports = router
